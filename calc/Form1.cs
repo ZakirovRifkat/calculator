53c397287b9c04d9bc2aac316b107a5b38996c6a
Zakirov Rifkat <zakirov2323rifkat@gmail.com>
2023-05-28 18:38:32 +0300

    check solution

--- calc/Form1.cs
+++ calc/Form1.cs
@@ -5,6 +5,7 @@ namespace calc
     public partial class Form1 : Form
     {
         bool isClickSolution = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -29,49 +30,57 @@ namespace calc
                 }
             }
         }
-        private async void StartCheckingAsync()
+
+        private void checkClickSolution() 
         {
-            await CheckTextBoxAsync(); // Запускаем бесконечный цикл проверки текста в textBox
-        }
-
-
-        private void one_Click(object sender, EventArgs e)
-        {
-
             if (isClickSolution == true)
             {
                 isClickSolution = false;
                 input.Text = "";
             }
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+
+            checkClickSolution();
             input.Text += "1";
 
         }
+
         private void zero_Click(object sender, EventArgs e)
         {
+            checkClickSolution();
             input.Text += "0";
         }
+
         private void dot_Click(object sender, EventArgs e)
         {
+            checkClickSolution();
             input.Text += ",";
         }
 
         private void plus_Click(object sender, EventArgs e)
         {
+            checkClickSolution();
             input.Text += "+";
         }
 
         private void minus_Click(object sender, EventArgs e)
         {
+            checkClickSolution();
             input.Text += "-";
         }
 
         private void multyply_Click(object sender, EventArgs e)
         {
+            checkClickSolution();
             input.Text += "*";
         }
 
         private void division_Click(object sender, EventArgs e)
         {
+            checkClickSolution();
             input.Text += "/";
         }
 
@@ -85,25 +94,26 @@ namespace calc
             {
                 solution = Convert.ToString(dt.Compute(expression, ""));
             }
-            catch (Exception exeption) {
-                MessageBox.Show(exeption.Message,
-                        "Предупреждение",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Warning);
+            catch (Exception exeption) 
+            {
+                MessageBox.Show(exeption.Message, 
+                    "Предупреждение", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Warning
+                    );
             }
             input.Text = solution;
-
         }
 
 
         private void allClear_Click(object sender, EventArgs e)
         {
-
+            input.Text = "";
         }
 
         private void clear_Click(object sender, EventArgs e)
         {
-
+            input.Text = input.Text.Remove(input.Text.Length - 1);
         }
 
         private void leftBracket_Click(object sender, EventArgs e)
